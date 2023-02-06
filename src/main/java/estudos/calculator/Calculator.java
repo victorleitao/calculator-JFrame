e74d2e6390c76e79f39e46081889a4d2e4d2ad28
victorleitao <victorleitao1991@hotmail.com>
2023-02-06 18:27:00 -0300

    first commit

---
+++ src/main/java/estudos/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package estudos.calculator;
+
+import view.MainScreen;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        MainScreen mainScreen = new MainScreen();
+        mainScreen.setVisible(true);
+    }
+}
